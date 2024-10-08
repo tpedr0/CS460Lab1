f66d84580266624aee1581aee3af708bec6c41a9
Pedro <pedro.teodoro@cwu.edu>
2024-10-07 17:45:32 -0700

    Add javascript functionality

--- app/src/main/java/com/example/lab1calculator/MainActivity.java
+++ app/src/main/java/com/example/lab1calculator/MainActivity.java
@@ -12,6 +12,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.button.MaterialButton;
 
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
 import java.time.format.TextStyle;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -22,9 +25,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     MaterialButton button0, button1, button2, button3, button4, button5, button6, button7, button8, button9;
 
-    MaterialButton buttonMul, buttonPlus, buttonSub, buttonDiv, ButtonEquals;
+    MaterialButton buttonMul, buttonPlus, buttonSub, buttonDiv, buttonEquals;
 
     MaterialButton buttonAC, buttonDot;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,14 +42,81 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         resultTV = findViewById(R.id.result_tv);
         solutionTV = findViewById(R.id.solution_tv);
+
+        assignID(buttonC, R.id.button_c);
+        assignID(buttonBrackOpen, R.id.button_open_bracket);
+        assignID(buttonBrackClose, R.id.button_close_bracket);
+
+        assignID(button0, R.id.button_0);
+        assignID(button1, R.id.button_1);
+        assignID(button2, R.id.button_2);
+        assignID(button3, R.id.button_3);
+        assignID(button4, R.id.button_4);
+        assignID(button5, R.id.button_5);
+        assignID(button6, R.id.button_6);
+        assignID(button7, R.id.button_7);
+        assignID(button8, R.id.button_8);
+        assignID(button9, R.id.button_9);
+
+        assignID(buttonMul, R.id.button_mul);
+        assignID(buttonPlus, R.id.button_plus);
+        assignID(buttonDiv, R.id.button_division);
+        assignID(buttonSub, R.id.button_sub);
+        assignID(buttonEquals, R.id.button_equals);
+
+        assignID(buttonAC, R.id.button_AC);
+        assignID(buttonDot, R.id.button_dot);
+
+
+
+
     }
 
     void assignID(MaterialButton btn, int id) {
         btn = findViewById(id);
+        btn.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View v) {
+    public void onClick(View view) {
+        MaterialButton button = (MaterialButton) view;
+        String buttonText = button.getText().toString();
+        String dataToCalculate = solutionTV.getText().toString();
 
+        if(buttonText.equals("AC")) {
+            solutionTV.setText("");
+            resultTV.setText("0");
+            return;
+        }
+
+        if(buttonText.equals("=")){
+            solutionTV.setText(resultTV.getText());
+            return;
+        }
+
+        if(buttonText.equals("C")) {
+            dataToCalculate = dataToCalculate.substring(0,dataToCalculate.length()-1);
+        } else {
+            dataToCalculate = dataToCalculate + buttonText;
+        }
+
+        solutionTV.setText(dataToCalculate);
+
+        String finalResult = getResults(dataToCalculate);
+
+        if(!finalResult.equals("Err")) {
+            resultTV.setText(finalResult);
+        }
+    }
+
+    String getResults(String data) {
+        try {
+            Context context = Context.enter();
+            context.setOptimizationLevel(-1);
+            Scriptable scriptable = context.initStandardObjects();
+            return context.evaluateString(scriptable, data, "Javascript", 1, null).toString();
+        } catch (Exception e){
+            return "Err";
+        }
     }
 }
